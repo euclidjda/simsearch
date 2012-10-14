6aa2902a6913a5d1ff83b2bbbc8d6ca11255b1e5
ferhan <ferhan@netarota>
2012-10-14 16:14:34 -0700

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simsearch::Application.config.secret_token = 'd448a6f83d2e0d56aa6f3858db25d6149381eb3882617b656f95309cc0caa3c49c5f17f8db75e9f4f2bed0ae467889235fd3099f72ff1de4546919a146759065'
