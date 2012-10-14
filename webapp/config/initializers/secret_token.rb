28d0eca956847834242a676e723a7a7e5df87a86
ferhan <ferhan@netarota>
2012-10-14 10:42:51 -0700

    First checkin with the rails app scaffolding

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '8706e782ae8c8a5008f969d6808649ef38655fc5263400f1d6b66d376240c559a995c10cea0713635660e8e1bb76f172d39cdedad2e2abcd6f3fe95fea9aad43'
