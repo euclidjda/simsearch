0687294f81789a5cb3d0f5361efb445f4f766e86
Ferhan Elvanoglu <ferhane@gmail.com>
2012-10-20 21:53:54 -0700

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+class User < ActiveRecord::Base
+=begin
+  #if we use mongo / mongoid one day, we would use this notation. 
+
+  field :uid, :type => String
+  field :provider, :type => String
+  field :email, :type => String
+  field :name, :type => String
+  field :first_name, :type => String
+  field :last_name, :type => String
+  field :oauth_token, :type => String
+  field :role, :type => Integer
+=end
+
+  # Make all of our attributes bulk accessible to our code now since we don't
+  # have a controller for user, this is safe.
+  attr_accessible :uid, :provider, :email, :name, :first_name, :last_name, :oauth_token, :role
+
+  # return the display name of the user.
+  def display_name 
+  	name
+  end
+
+  def self.create_with_email(email)
+    logger.info "Creating user with email: " + email
+
+    # try to find the user first
+    user = find_by_email(email)
+
+    # If we could not find the user, we create one with the email, but we have no details.
+    # This user is basically requesting access to the service. We will get his details through
+    # a separate form later. When we do Google / LinkedIn, etc. auth, we will get name details from
+    # those providers' profile of the user.
+    if ! user
+      user = create! do |user|
+        user.email = email
+        user.provider = "manual"  # Indicating this was a manual user information entry.
+      end
+    end
+
+    # return the user 
+    user
+  end
+
+private
+  def find_by_email(email)
+  	begin
+  	  return self.find_by(:mail => email)
+    rescue
+ 	    return nil
+    end
+  end
+
+end
---
+++ db/migrate/20121021031616_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :uid
+      t.string :provider
+      t.string :email
+      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :oauth_token
+
+      t.timestamps
+    end
+  end
+end
