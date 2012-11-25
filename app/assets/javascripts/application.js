222b14e5800b7c9959dece14e33e6ee5e383548f
Ferhan Elvanoglu <ferhane@gmail.com>
2012-11-25 13:42:22 -0800

    heroku fix for script:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require twitter/bootstrap
 //= require jquery-1.8.2.min
 //= require jquery-ui-1.9.1.custom.min 
+//= require twitter/bootstrap
 //= require_tree .
