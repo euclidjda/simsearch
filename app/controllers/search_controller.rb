066f0e22e1fc7bad2e5afa6d4e5c36998c61c9e6
ferhan <ferhane@gmail.com>
2012-12-01 21:48:02 -0800

    more deliberate naming of select statement, so we can use the same structure with minimal if/else additions for filters as well.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,14 +9,14 @@ class SearchController < ApplicationController
   def autocomplete_investment_ticker
     term = params[:term]
     if term && !term.empty?
-      items = Investment.select("distinct id, ticker, name").
+      items = Investment.select("distinct id, ticker as shortname, name as longname").
           where("LOWER(CONCAT(ticker, name)) like ?", '%' + term.downcase + '%').
-          limit(10).order(:ticker)
+          limit(10).order(:shortname)
     else
       items = {}
     end
 
-    render :json => json_for_autocomplete(items, :ticker, [:id, :name])
+    render :json => json_for_autocomplete(items, :shortname, [:id, :longname])
     
   end
 
