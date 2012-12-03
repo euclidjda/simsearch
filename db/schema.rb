6d543bc31c1a7b9adce6737ca0bd53b100b4ab06
John Alberg <john.alberg@euclidean.com>
2012-12-02 19:25:44 -0500

    This is the change version of db/schema.rb. Fehan/Alberg need to think through how this process should work. This file shouldn't change unless there are actual db schema changes.

--- db/schema.rb
+++ db/schema.rb
@@ -13,22 +13,35 @@
 
 ActiveRecord::Schema.define(:version => 20121202082420) do
 
-  create_table "ex_factdata", :primary_key => "cid", :force => true do |t|
-    t.date    "fromdate",      :null => false
-    t.date    "thrudate",      :null => false
-    t.date    "datadate",      :null => false
-    t.integer "indidx",        :null => false
-    t.integer "lcapidx",       :null => false
-    t.integer "hcapidx",       :null => false
-    t.integer "lvalidx",       :null => false
-    t.integer "hvalidx",       :null => false
-    t.integer "ldividx",       :null => false
-    t.integer "hdividx",       :null => false
+  create_table "ex_econ", :id => false, :force => true do |t|
+    t.date  "datadate", :null => false
+    t.float "cape"
+    t.float "tbill6mo"
+    t.float "note10yr"
+  end
+
+  add_index "ex_econ", ["datadate"], :name => "ex_econ_ix01"
+
+  create_table "ex_factdata", :id => false, :force => true do |t|
+    t.string  "cid",           :limit => 6, :null => false
+    t.string  "sid",           :limit => 3, :null => false
+    t.date    "fromdate",                   :null => false
+    t.date    "thrudate",                   :null => false
+    t.date    "datadate",                   :null => false
+    t.integer "idxind",                     :null => false
+    t.integer "idxdiv",                     :null => false
+    t.integer "idxnew",                     :null => false
+    t.integer "idxcapl",                    :null => false
+    t.integer "idxcaph",                    :null => false
+    t.integer "idxvall",                    :null => false
+    t.integer "idxvalh",                    :null => false
     t.float   "dvpsxm_ttm"
     t.float   "epspiq_ttm"
+    t.float   "epspxq_ttm"
     t.float   "epspiq_10yISr"
     t.float   "niq_ttm"
     t.float   "oiadpq_ttm"
+    t.float   "cogsq_ttm"
     t.float   "saleq_ttm"
     t.float   "saleq_4yISgx"
     t.float   "seqq_mrq"
@@ -36,12 +49,14 @@ ActiveRecord::Schema.define(:version => 20121202082420) do
     t.float   "dlttq_mrq"
     t.float   "dlcq_mrq"
     t.float   "pstkq_mrq"
+    t.float   "miiq_ttm"
   end
 
-  add_index "ex_factdata", ["cid", "fromdate", "thrudate"], :name => "ex_factdata_ix01"
-  add_index "ex_factdata", ["indidx", "lcapidx", "hcapidx", "lvalidx", "hvalidx", "ldividx", "hdividx"], :name => "ex_factdata_ix02"
+  add_index "ex_factdata", ["cid", "sid", "fromdate", "thrudate"], :name => "ex_factdata_ix01"
+  add_index "ex_factdata", ["idxind", "idxdiv", "idxnew", "idxcapl", "idxcaph", "idxvall", "idxvalh"], :name => "ex_factdata_ix02"
 
-  create_table "ex_funddata", :primary_key => "cid", :force => true do |t|
+  create_table "ex_fundmts", :id => false, :force => true do |t|
+    t.string "cid",      :limit => 6, :null => false
     t.date   "fromdate",              :null => false
     t.date   "thrudate",              :null => false
     t.date   "datadate",              :null => false
@@ -104,7 +119,23 @@ ActiveRecord::Schema.define(:version => 20121202082420) do
     t.float  "fcfl"
   end
 
-  add_index "ex_funddata", ["cid", "fromdate", "thrudate", "type"], :name => "ex_funddata_01"
+  add_index "ex_fundmts", ["cid", "fromdate", "thrudate", "type"], :name => "ex_funddata_01"
+
+  create_table "ex_prices", :id => false, :force => true do |t|
+    t.string "cid",      :limit => 6, :null => false
+    t.string "sid",      :limit => 3, :null => false
+    t.date   "datadate",              :null => false
+    t.float  "csho"
+    t.float  "ajex"
+    t.float  "price"
+    t.float  "chng1m"
+    t.float  "chng3m"
+    t.float  "chng6m"
+    t.float  "chng9m"
+    t.float  "chng12m"
+  end
+
+  add_index "ex_prices", ["cid", "sid", "datadate"], :name => "ex_price_ix01"
 
   create_table "filters", :force => true do |t|
     t.string   "name"
