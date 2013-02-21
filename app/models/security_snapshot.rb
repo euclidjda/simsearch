b4d1f0e7e369cd7ca5ece82138fb836f49ac2802
John Alberg <john.alberg@euclidean.com>
2013-02-20 16:28:12 -0800

    Added :stream => true to mysql2 query in security_snapshot.rb. This is an attempt to better manage the very large query that is being processed and causing memory issues on heroku. It apparently also turns off query caching.

--- app/models/security_snapshot.rb
+++ app/models/security_snapshot.rb
@@ -218,7 +218,7 @@ class SecuritySnapshot < Tableless
     
     logger.debug sqlstr
 
-    defer1 = client1.query sqlstr
+    defer1 = client1.query(sqlstr,:stream => true)
 
     defer1.callback do |result|
       logger.debug "**** Calling callback for search_id: #{_search_id}"
