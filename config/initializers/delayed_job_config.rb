b31dd2e40e9978aaa21e5fddbc9b28a5c560b2c2
John Alberg <john.alberg@euclidean.com>
2013-04-10 17:43:42 -0500

    More debugging heroku, delayed_job.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,6 +2,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 2
 Delayed::Worker.max_attempts = 1
-Delayed::Worker.read_ahead = 1
+# Delayed::Worker.read_ahead = 1
 # Delayed::Worker.max_run_time = 5.minutes
 # Delayed::Worker.delay_jobs = !Rails.env.test?
