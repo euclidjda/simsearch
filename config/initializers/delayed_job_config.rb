7c4b9e8e7ecf3bdc7824ed76b0aee4b1ad5ff9f3
John Alberg <john.alberg@euclidean.com>
2013-04-10 14:25:19 -0500

    Changed delayed job config again.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,7 +1,7 @@
 # config/initializers/delayed_job_config.rb
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.sleep_delay = 5
-Delayed::Worker.max_attempts = 3
+Delayed::Worker.max_attempts = 1
 # Delayed::Worker.max_run_time = 5.minutes
 # Delayed::Worker.read_ahead = 10
 # Delayed::Worker.delay_jobs = !Rails.env.test?
