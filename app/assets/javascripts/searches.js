54e4fdee8fc9ae4177beefb8ce83d5982adab359
Ferhan Elvanoglu <ferhane@gmail.com>
2013-04-29 00:44:47 -0700

    All necessary wiring for share operation. We are doing everything other than rendering summary and sending the mail

--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,4 +1,10 @@
 
+  
+// Global variables for search actions page, that are used for current context 
+// throughout the UI.
+var global_search_id;
+var global_search_ticker;
+
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
@@ -18,11 +24,17 @@ function showSearchDetail(_action) {
   $("." + _action + "-detail").show();
 }
 
+function hideAlerts() {
+  $('.alert').hide();
+}
+
 function handleSearchAction(e, _action, _search_id) {
   // alert( _action + " : " + _search_id);
 
   e.preventDefault();
   e.stopPropagation();
+
+  hideAlerts();
 
   switch (_action) {
     case "favorite":
@@ -37,9 +49,32 @@ function handleSearchAction(e, _action, _search_id) {
       hideSearchInfo();
       showSearchDetail(_action);
 
-      // ShowShare();
-
       break;
   }
 }
 
+function updateGlobals(_search_id, _search_ticker) {
+  global_search_id = _search_id;
+  global_search_ticker = _search_ticker;
+  
+  $("#ticker-name").html(global_search_ticker.toUpperCase());
+
+  renderSearchDetails(_search_id, _search_ticker);
+}
+
+function handleListItemClick(e) {
+  hideSearchInfo();
+  showSearchDetail(getHash());
+  hideAlerts();
+
+  // read and cache the search id and ticker from the current selection.
+  var target = e.currentTarget;
+
+  updateGlobals($(target).attr("search-id"), $(target).attr("search-ticker"));
+  
+  renderSearchDetails(global_search_id, global_search_ticker);
+}
+
+function renderSearchDetails(_search_id, _search_ticker) {
+    $("#result-summary-content").html("Search ID: " + _search_id + "<br>" + "Ticker: " + _search_ticker);
+}
