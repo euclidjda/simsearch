fa1fea37d69c7f2826ec06e54b264729bb75802f
John Alberg <john.alberg@euclidean.com>
2013-09-27 12:43:43 -0500

    Update summary view css.

--- db/common_queries.sql
+++ db/common_queries.sql
@@ -15,3 +15,38 @@ AND B.pricedate='2013-05-31') C
 WHERE C.comps >= 20
 ORDER BY C.pcnt DESC limit 50;
 
+
+
+SELECT idxsub,COUNT(*) FROM (
+
+       SELECT idxsub,cid,sid,MAX(idxcaph) maxcap,MAX(datadate) maxdate
+       FROM ex_factdata 
+       GROUP BY idxsub,cid,sid
+
+) A 
+WHERE maxcap >= 1000
+AND maxdate BETWEEN '2000-01-01' AND '2020-12-31'
+GROUP BY idxsub;
+
+SELECT idxind,COUNT(*) FROM (
+
+       SELECT idxind,cid,sid,MAX(idxcaph) maxcap,MAX(datadate) maxdate
+       FROM ex_factdata 
+       GROUP BY idxind,cid,sid
+
+) A 
+WHERE maxcap >= 1000
+AND maxdate BETWEEN '2000-01-01' AND '2020-12-31'
+GROUP BY idxind;
+
+SELECT idxgrp,COUNT(*) FROM (
+
+       SELECT idxgrp,cid,sid,MAX(idxcaph) maxcap,MAX(datadate) maxdate
+       FROM ex_factdata 
+       GROUP BY idxgrp,cid,sid
+
+) A 
+WHERE maxcap >= 1000
+AND maxdate BETWEEN '2000-01-01' AND '2020-12-31'
+GROUP BY idxgrp;
+
