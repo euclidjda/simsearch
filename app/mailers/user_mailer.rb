1446f49354528c561a2a6627b0a030a37d94cb1d
John Alberg <john.alberg@euclidean.com>
2014-01-29 10:02:57 -0500

    Fixed url bug in mailer.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(_user)
     @user = _user
-    @url  = "http://www.euclidean.com/fundamentals"
+    @url  = "http://fundamentals.euclidean.com"
     mail( :to => @user.email, 
           :subject => "Welcome to Euclidean Fundamentals")
   end
 
   def share_email(_user, _target, _search_id, _company_name, _message, _overunder)
     @user = _user
-    @url = "http://www.euclidean.com/fundamentals?search=" + _search_id
+    @url = "http://fundamentals.euclidean.com?search=" + _search_id
     @company_name = _company_name
     @message = _message
     @overunder = _overunder
